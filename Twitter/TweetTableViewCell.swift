d9c56d1f10b0b82907a537305c880e23f9ea4433
w3qiu <damon.q26@gmail.com>
2019-03-04 22:46:09 -0800

    finish basic twitter with log in log out fetch tweets

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by wenlong qiu on 3/4/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
